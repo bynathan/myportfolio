5ccdbcd7c81b354b5abc4a6eccadc06f0dc0b751
bynathan <nathancontato14112003@gmail.com>
2023-01-25 12:47:55 -0300

    redirecTo for home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 
 export const portfolioRoutes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'projects', component: ProjectsComponent},
   {path: 'about-me', component: AboutComponent},
